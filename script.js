ea7915c36e1a5932120ca7743472cc082e2ddf2a
Gaurav Mishra <gauravmishraokok@gmail.com>
2024-01-21 16:33:54 +0530

    Created bulb project

---
+++ script.js
@@ -0,0 +1,31 @@
+//bulb
+//button 
+//click 
+
+let bulb = document.getElementById('bulb')
+
+let button = document.querySelector('#button')
+
+
+let click = 0;
+
+
+button.addEventListener('click' , function(){
+    //odd --> ON
+    //even -- ON
+    click++;
+    console.log('click' , click)
+    
+    if(click % 2 ==0) {
+        //even -- off 
+        bulb.style.backgroundColor  = 'white'
+        button.textContent ="ON"
+    } 
+    else {
+        bulb.style.backgroundColor  = 'yellow'
+        button.textContent ="OFF"
+
+    }
+})
+
+
